af7ad02d1b627aea38d78cc0fd599bef4c47cc0a
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2023-04-01 07:21:37 +0200

    base crawler with multi processing

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -7,46 +7,52 @@ import pandas as pd
 import tqdm
 import time
 from tqdm.contrib import tzip
+import traceback
 
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-# options.add_argument("--headless")
-
-driver = webdriver.Chrome(options=options)
-download_image_root = "../images/"
+if __name__=="__main__":
+    driver = crawling_utils.init_driver()
+    download_image_root = "dataset/images/"
+    df_path = "dataset/digimon_dataset.csv"
 
 
-web_site_url = "https://wikimon.net/Visual_List_of_Digimon"
-driver.get(web_site_url)
+    web_site_url = "https://wikimon.net/Visual_List_of_Digimon"
+    driver.get(web_site_url)
 
-# GET ALL IMAGES WITH LINKS OF THE DIGILIST
-tag_images = "//a//img[@decoding=\"async\"]"
-crawling_utils.wait_until_xpath(driver, tag_images)
-digimon_a_href = driver.find_elements(By.XPATH, tag_images)
+    # GET ALL IMAGES WITH LINKS OF THE DIGILIST
+    tag_images = "//a//img[@decoding=\"async\"]"
+    crawling_utils.wait_until_xpath(driver, tag_images)
+    digimon_a_href = driver.find_elements(By.XPATH, tag_images)
 
-print(len(digimon_a_href))
+    print(len(digimon_a_href))
 
-dataset = pd.DataFrame(
-    columns=["name", "full_page_wiki_url", "image_filename", "description"])
+    dataset = pd.DataFrame(
+        columns=["name", "full_page_wiki_url", "image_url", "image_filename", "description"])
 
-pairs = []
-for el in tqdm.tqdm(digimon_a_href, total=len(digimon_a_href), desc="parsing urls"):
-    name = el.get_attribute("alt")
-    if len(name.split()) > 1:
-        name = name.split()[0] + "_" + " ".join(name.split()[1:])
-    url_page = "https://wikimon.net/%s" % name
-    pairs.append((name, url_page))
+    pairs = []
+    for el in tqdm.tqdm(digimon_a_href, total=len(digimon_a_href), desc="parsing urls"):
+        name = el.get_attribute("alt")
+        if len(name.split()) > 1:
+            name = name.split()[0] + "_" + " ".join(name.split()[1:])
+        url_page = "https://wikimon.net/%s" % name
+        pairs.append((name, url_page))
 
-for (name, url_page) in tqdm.tqdm(pairs, total=len(pairs), desc="Retrieving Image and Text"):
-    try:
-        image_path, image_url, description = crawling_utils.get_image_text_from_url(
-            driver, url_page, name, download_image_root)
-        new_row = {"name": name, "full_page_wiki_url": url_page, "image_url": image_url,
-                                 "image_path": image_path, "description": description}
-        dataset = dataset.append(new_row, ignore_index=True)
-        dataset.to_csv("test_df.csv", index=False)
 
-    except Exception as e:
-        print(e)
-        continue
+    driver.close()
+    crawling_utils.create_driver_parallel_crawling(pairs,download_image_root)
 
+# fails = []
+# for (name, url_page) in tqdm.tqdm(pairs, total=len(pairs), desc="Retrieving Image and Text"):
+#     try:
+#         image_path, image_url, description = crawling_utils.get_image_text_from_url(
+#             driver, url_page, name, download_image_root)
+#         new_row = {"name": name, "full_page_wiki_url": url_page, "image_url": image_url,
+#                                  "image_filename": image_path, "description": description}
+#         dataset = dataset.append(new_row, ignore_index=True)
+#         dataset.to_csv(df_path, index=False)
+
+#     except Exception as e:
+#         print(e)
+#         fails.append((name, url_page))
+#         with open("dataset/fails.txt", "w") as f:
+#             for x in fails:
+#                 f.write(",".join(x) + "\n")
--- crawler/crawling_utils.py
+++ crawler/crawling_utils.py
@@ -1,11 +1,24 @@
 import time
 import os
+from selenium import webdriver  # Import from seleniumwire
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import urllib.request
 import requests
+from cleantext import clean
+import multiprocessing as mp
+import tqdm 
+import pandas as pd
 
+list_to_empty_token = ["Digimon Reference Book","Video Games Misc","Anime & Manga","\n"]
+
+def init_driver():
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument("--headless")
+    options.add_argument("--log-level=3")
+    driver = webdriver.Chrome(options=options)
+    return driver 
 
 def wait_until_xpath(driver, xpath, delay=3):
     delay = delay  # seconds
@@ -24,6 +37,7 @@ def download_image(url, image_path_save):
 
 
 def get_image_text_from_url(driver, url_page, name, download_image_root):
+    # print("CURRENT URL %s" % url_page)
     driver.get(url_page)
     time.sleep(1)
     # DESCRIPTION
@@ -31,24 +45,29 @@ def get_image_text_from_url(driver, url_page, name, download_image_root):
     wait_until_xpath(driver, desc_tag)
     description_td = driver.find_elements(
         By.XPATH, desc_tag)
+    # print([x.text for x in description_td])
+    # THIS TOKENIZATION IS CORRECT, THE FAILS ARE RELATED TO DIGIMON PAGES WITH NO DESCRIPTION,
+    # THAT WILL BE SKIPPED
     description = [x.text for x in description_td if hasattr(
         x, "text") and "⇨ Japanese" in x.text]
-    description = description[0].split("\n")
-    i = description.index("⇨ Japanese")
-    description = " ".join(
-        description[i+1:]).replace("Digimon Reference Book", "").strip()
+
+    # mege the text
+    description = " ".join(description)
+    description = "".join(description.split("⇨ Japanese")[1])
+    description = clean(description)
+    for bw in list_to_empty_token:
+        description = description.replace(bw.lower(), "").strip() # lower because i previously cleaned the sentence
 
     # IMAGE
     image_desc = "//tbody//tr//td[@colspan=\"4\"]//div//div//a[@class=\"image\"]//img"
     image_url = driver.find_element(By.XPATH, image_desc).get_attribute("src")
     image_url = os.path.basename(image_url)
-
-    print(image_url)
+    # print(image_url)
     if "-" in image_url:
         image_url = "-".join(os.path.basename(image_url).split("-")[1:])
     real_basename = image_url
     image_url = "https://wikimon.net/File:" + image_url
-    print(image_url)
+    # print(image_url)
     # IMAGE PAGE
     driver.get(image_url)
     desc_tag = "//div[@class=\"fullImageLink\"]//a"
@@ -56,7 +75,39 @@ def get_image_text_from_url(driver, url_page, name, download_image_root):
     time.sleep(1)
     image_file_url = driver.find_element(
         By.XPATH, desc_tag).get_attribute("href")
-    print(image_file_url)
-    image_path = "%s/%s" % (download_image_root,real_basename) 
+    # print(image_file_url)
+    image_path = "%s/%s" % (download_image_root, real_basename)
     download_image(image_file_url, image_path)
     return real_basename, image_url, description
+
+
+def crawl_data(pairs, download_image_root):
+    driver = init_driver()
+    fails = []
+    dataset = pd.DataFrame(columns=["name", "full_page_wiki_url", "image_url", "image_filename", "description"])
+    for (name, url_page) in tqdm.tqdm(pairs, total=len(pairs), desc="Retrieving Image and Text"):
+        try:
+            image_path, image_url, description = get_image_text_from_url(
+                driver, url_page, name, download_image_root)
+            new_row = {"name": name, "full_page_wiki_url": url_page, "image_url": image_url,
+                                    "image_filename": image_path, "description": description}
+            dataset = dataset.append(new_row, ignore_index=True)
+            # dataset.to_csv(df_path, index=False)
+
+        except Exception as e:
+            print(e)
+            fails.append((name, url_page))
+            with open("dataset/fails.txt", "w") as f:
+                for x in fails:
+                    f.write(",".join(x) + "\n")
+    return dataset, fails
+
+def create_driver_parallel_crawling(pairs, download_image_root):
+    cp_count = mp.cpu_count()
+    print(cp_count)
+    partition_len = len(pairs)//cp_count
+    pool = mp.Pool(cp_count)
+    partitions = [pairs[i:(i+partition_len)] for i in range(0, len(pairs), partition_len)]
+    results = pool.starmap(crawl_data, [(part, download_image_root) for part in partitions])
+    print(len(results))
+    pool.close()
